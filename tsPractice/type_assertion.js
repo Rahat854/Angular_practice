e98d518e74dc61713ce7704d4d392fa5e2dd8e47
rahat854 <rahatkabir20@gmail.com>
2021-07-02 21:17:38 +0600

    type_assertion

---
+++ tsPractice/type_assertion.js
@@ -0,0 +1,4 @@
+var message;
+message = 'abc';
+var endsWithC = message.endsWith > message.endsWith('c');
+var alternativeWay = message.endsWith('c');
---
+++ tsPractice/type_assertion.ts
@@ -0,0 +1,4 @@
+let message;
+message = 'abc'
+let endsWithC =(<string>message).endsWith>message.endsWith('c')
+let alternativeWay = (message as string).endsWith('c')
