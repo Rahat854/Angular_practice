e9ae3ebb068497595630dce30165348f9cf7164b
rahat854 <rahatkabir20@gmail.com>
2021-07-04 21:22:05 +0600

    added property binding

--- angular-practice/src/app/courses.component.ts
+++ angular-practice/src/app/courses.component.ts
@@ -4,20 +4,14 @@ import { Component } from "@angular/core";
   selector: 'courses',
   template: `
   <h2>{{ title }}</h2>
-  <ul>
-    <li *ngFor = "let course of courses">
-      {{course}}
-    </li>
-  </ul>
+  <h2 [textContent] = "title"></h2>
+  <img src = "{{imageUrl}}"/>
+  <img [src] = "title" />
   `
 })
 export class CoursesComponent {
   title = "List of courses"
-  courses
-
-  constructor(service: CourseService) {
-    this.courses = service.getCourses()
-  }
+  imageUrl = "https://images.pexels.com/photos/5391353/pexels-photo-5391353.jpeg?cs=srgb&dl=pexels-antonio-prado-5391353.jpg&fm=jpg"
 
   //Logic for calling an HTTP service
 }
