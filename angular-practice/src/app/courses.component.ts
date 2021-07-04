77fd080ecbc2f0ba15ca1212156a98306413a84b
rahat854 <rahatkabir20@gmail.com>
2021-07-04 21:29:53 +0600

    atrribute binding

--- angular-practice/src/app/courses.component.ts
+++ angular-practice/src/app/courses.component.ts
@@ -4,14 +4,17 @@ import { Component } from "@angular/core";
   selector: 'courses',
   template: `
   <h2>{{ title }}</h2>
-  <h2 [textContent] = "title"></h2>
   <img src = "{{imageUrl}}"/>
-  <img [src] = "title" />
+  <table>
+    <tr>
+      <td [attr.colSpan] = "colSpan"></td>
+    </tr>
+  </table>
   `
 })
 export class CoursesComponent {
   title = "List of courses"
   imageUrl = "https://images.pexels.com/photos/5391353/pexels-photo-5391353.jpeg?cs=srgb&dl=pexels-antonio-prado-5391353.jpg&fm=jpg"
-
+  colSpan = 2
   //Logic for calling an HTTP service
 }
