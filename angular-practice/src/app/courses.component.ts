d80c133627af21fe0d383a7943d6ffda72038ecb
rahat854 <rahatkabir20@gmail.com>
2021-07-05 00:53:54 +0600

    event filtering

--- angular-practice/src/app/courses.component.ts
+++ angular-practice/src/app/courses.component.ts
@@ -3,22 +3,26 @@ import { Component } from "@angular/core";
 @Component({
   selector: 'courses',
   template: `
-  <h2>{{ title }}</h2>
+  <!-- <h2>{{ title }}</h2>
   <div (click) = "onDivClicked()">
   <button (click) = "onSave($event)">Add</button>
-  </div>
+  </div> -->
+  <input (keyup.enter) = "onKeyUp()" />
   `
 })
 export class CoursesComponent {
-  title = "List of courses"
-  colSpan = 2
-  isActive = true
-  onDivClicked() {
-    console.log('Div is clicked')
+  // title = "List of courses"
+  // colSpan = 2
+  // isActive = true
+  // onDivClicked() {
+  //   console.log('Div is clicked')
+  // }
+  // onSave($event: any) {
+  //   $event.stopPropagation()
+  //   console.log('Button is clicked',$event)
+  // }
+  // //Logic for calling an HTTP service
+  onKeyUp() {
+      console.log('enter was pressed')
   }
-  onSave($event: any) {
-    $event.stopPropagation()
-    console.log('Button is clicked',$event)
-  }
-  //Logic for calling an HTTP service
 }
