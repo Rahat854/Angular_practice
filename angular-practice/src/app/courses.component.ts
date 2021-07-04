2a53abd4aafc524a37c02e6a9aea75fc4a80e7a4
rahat854 <rahatkabir20@gmail.com>
2021-07-05 00:31:55 +0600

    added bootstrap

--- angular-practice/src/app/courses.component.ts
+++ angular-practice/src/app/courses.component.ts
@@ -4,17 +4,11 @@ import { Component } from "@angular/core";
   selector: 'courses',
   template: `
   <h2>{{ title }}</h2>
-  <img src = "{{imageUrl}}"/>
-  <table>
-    <tr>
-      <td [attr.colSpan] = "colSpan"></td>
-    </tr>
-  </table>
+  <button class = "btn btn-primary">Add</button>
   `
 })
 export class CoursesComponent {
   title = "List of courses"
-  imageUrl = "https://images.pexels.com/photos/5391353/pexels-photo-5391353.jpeg?cs=srgb&dl=pexels-antonio-prado-5391353.jpg&fm=jpg"
   colSpan = 2
   //Logic for calling an HTTP service
 }
