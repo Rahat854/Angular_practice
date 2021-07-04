b2b99dd826a341edfdd931abb15c70f52efebddc
rahat854 <rahatkabir20@gmail.com>
2021-07-04 20:42:12 +0600

    added components

--- angular-practice/src/app/app.module.ts
+++ angular-practice/src/app/app.module.ts
@@ -1,13 +1,15 @@
+import { CoursesComponent } from './courses.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { CoursesComponent } from './courses.component';
+import { CourseComponent } from './course/course.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CourseComponent,
     CoursesComponent
   ],
   imports: [
