b4988198928f8cb5de18e8d66bf1e122ff057a95
rahat854 <rahatkabir20@gmail.com>
2021-07-12 15:48:23 +0600

    added dropdown

--- angular-practice/src/app/contact-form/contact-form.component.ts
+++ angular-practice/src/app/contact-form/contact-form.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'contact-form',
@@ -6,6 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.scss']
 })
 export class ContactFormComponent {
+  contactMethods = [
+    {id: 1, name: 'Email'},
+    {id: 2, name: 'Phone'}
+
+  ]
   log(x: any){
     console.log(x)
   }
